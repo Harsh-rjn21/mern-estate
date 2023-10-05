4f9fbc1b8ac0c2c0d0da767b7a5a89c8246f17c1
Harsh Ranjan <harsn.ranjan.cse21@itbhu.ac.in>
2023-10-05 10:41:33 +0530

    created sign-up api route

--- api/index.js
+++ api/index.js
@@ -1,23 +1,27 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoute from "./routes/user.route.js"
+import userRoute from "./routes/user.route.js";
+import signUpRoute from "./routes/auth.route.js";
 
 dotenv.config();
 
-
-mongoose.connect(process.env.MONGO)
-    .then(()=>{
-        console.log("connected to mongoDB");
-    }).catch((err)=>{
-        console.log(err);
-    });
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.use("/api/user",userRoute);
+app.use(express.json());
 
+app.listen(3000, () => {
+  console.log("Server is running on 3000!");
+});
 
-app.listen(3000,()=>{
-    console.log("Server is running on 3000!");
-})
+app.use("/api/user", userRoute);
+app.use("/api/auth", signUpRoute);
